a71e2ef6236e1d5307cf7ade5d33237916531e21
Yingjie.Lu <Yingjie.Lu@geely.com>
2020-06-13 11:24:01 +0800

    修改默认的url是空数组

--- src/main/java/com/lyj/swagger2markdown/annotation/EnableSwaggerToMD.java
+++ src/main/java/com/lyj/swagger2markdown/annotation/EnableSwaggerToMD.java
@@ -12,6 +12,6 @@ import java.lang.annotation.*;
 @Import({SwaggerToMdAutoConfiguration.class, URLParam.class})
 public @interface EnableSwaggerToMD {
 
-    String[] url() default "";
+    String[] url() default {};
 
 }
